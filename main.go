d3f3a34c377b78d587a21656c289e817e818c9b1
Jean <acalto2@gmail.com>
2023-09-01 22:54:10 +0200

    commit message

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/dog"
+	"github.com/GoesToEleven/puppy"
+)
+
+
+
+func main() {
+	fmt.Println("Hello world")
+	var s=puppy.Bark()
+	fmt.Println(s)
+
+	var s1=dog.WhenGrownUp(s)
+	fmt.Println(s1)
+	puppy.From13()
+	puppy.From12()
+}
